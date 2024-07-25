d11053b444759a99de07fb09701f2c719af0aaed
senders-olek <gmavery95@proton.me>
2024-07-25 19:26:40 +0100

    Simulated RCE endpoint added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import subprocess
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import os
 
 import dotenv
@@ -23,11 +23,14 @@ async def test_env_injection():
 
     return {"message": f"env is {os.getenv('SAMPLE_ENV', None)}"}
 
-@app.get("/rce/{command}")
-async def rce(command: str):
+@app.post("/rce")
+async def rce(cmd: str):
     try:
-        output = subprocess.check_output(command, shell=True, text=True)
+        output = subprocess.check_output(cmd, shell=True, text=True)
+        print(cmd)
         return {"output": output}
     except subprocess.CalledProcessError as e:
         return {"error": str(e)}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
